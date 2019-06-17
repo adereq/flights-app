53d6b9da11bb017532272ddcc70b62a66264b9a6
Adrian Kupiszewski <kupiszewskiadrian@gmail.com>
2019-06-18 01:47:51 +0200

    Added bootstrap

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,33 @@
 module ApplicationHelper
+    def nav_items
+    [ {
+      url: "#", 
+      title: 'Home'
+    },
+    {
+      url: "#", 
+      title: 'About'
+    },
+    {
+      url: "#", 
+      title: 'Contact'
+    },
+    {
+      url: "#", 
+      title: 'Blog'
+    },
+    {
+      url: "#", 
+      title: 'Portfolio'
+    }
+  ]
+  end
+  
+  def nav_helper style, tag_type
+    nav_links = ''
+    nav_items.each do |item|
+      nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style}'>#{item[:title]}</a></#{tag_type}>"
+    end
+    nav_links.html_safe
+  end
 end
