399c9adb4ea750f928d2dc12f6719759375cceb3
Adrian Kupiszewski <kupiszewskiadrian@gmail.com>
2019-06-26 21:17:34 +0200

    implemented ticket page

--- app/controllers/concerns/current_cart.rb
+++
@@ -1,9 +0,0 @@
-module CurrentCart
-  private
-   def set_cart
-   	 @cart = Cart.find(session[:card_id])
-   rescue ActiveRecord::RecordNotFound
-     @cart = Cart.create
-     session[:cart_id] = @cart.cart_id
-   end
- end
