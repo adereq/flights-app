66c2fdd415e45fb51c67d8c99b65b6da6f55742f
Adrian Kupiszewski <kupiszewskiadrian@gmail.com>
2019-12-21 02:34:24 +0100

    created tickets with passengers details

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -18,20 +18,46 @@ class BookingsController < ApplicationController
         if @booking.booking_class == "economy"
           Flight.seat_economy_decrease(@booking.flight_id, @booking.passengers)
           for i in 1..@booking.passengers 
-            puts "Stworzono ticket"
-            @a = "@booking.first_name_#{i}".to_sym
-            puts a
-            @ticket = Ticket.new(booking_id: @booking.id, flight_id: @booking.flight_id, user_id: @booking.user_id, first_name: instance_variable_get(@a))
-            @ticket.save
+            if i == 1
+              @ticket = Ticket.new(booking_id: @booking.id, flight_id: @booking.flight_id, user_id: @booking.user_id, first_name: @booking.first_name_1,
+                last_name: @booking.last_name_1, pnr: @booking.pnr, price: @booking.price_1, seat_class: "economy")
+              @ticket.save
+              elsif i == 2
+              @ticket = Ticket.new(booking_id: @booking.id, flight_id: @booking.flight_id, user_id: @booking.user_id, first_name: @booking.first_name_2,
+                last_name: @booking.last_name_2, pnr: @booking.pnr, price: @booking.price_2, seat_class: "economy"))
+              @ticket.save
+              elsif i == 3
+              @ticket = Ticket.new(booking_id: @booking.id, flight_id: @booking.flight_id, user_id: @booking.user_id, first_name: @booking.first_name_3,
+                last_name: @booking.last_name_3, pnr: @booking.pnr, price: @booking.price_3, seat_class: "economy"))
+              @ticket.save    
+              elsif i == 4
+              @ticket = Ticket.new(booking_id: @booking.id, flight_id: @booking.flight_id, user_id: @booking.user_id, first_name: @booking.first_name_4,
+                last_name: @booking.last_name_4, pnr: @booking.pnr, price: @booking.price_4, seat_class: "economy"))
+              @ticket.save  
+            end           
           end
           format.html {redirect_to @booking, notice: 'Flight was succesfully created'}
         elsif @booking.booking_class == "business"
           Flight.seat_business_decrease(@booking.flight_id, @booking.passengers)
-            for i in 1..@booking.passengers 
-              puts "Stworzono ticket"
-              @ticket = Ticket.new(booking_id: @booking.id,flight_id: @booking.flight_id, user_id: @booking.user_id)
+          for i in 1..@booking.passengers 
+            if i == 1
+              @ticket = Ticket.new(booking_id: @booking.id, flight_id: @booking.flight_id, user_id: @booking.user_id, first_name: @booking.first_name_1,
+                last_name: @booking.last_name_1, pnr: @booking.pnr, price: @booking.price_1, seat_class: "business")
               @ticket.save
-             end
+              elsif i == 2
+              @ticket = Ticket.new(booking_id: @booking.id, flight_id: @booking.flight_id, user_id: @booking.user_id, first_name: @booking.first_name_2,
+                last_name: @booking.last_name_2, pnr: @booking.pnr, price: @booking.price_2, seat_class: "business"))
+              @ticket.save
+              elsif i == 3
+              @ticket = Ticket.new(booking_id: @booking.id, flight_id: @booking.flight_id, user_id: @booking.user_id, first_name: @booking.first_name_3,
+                last_name: @booking.last_name_3, pnr: @booking.pnr, price: @booking.price_3, seat_class: "business"))
+              @ticket.save    
+              elsif i == 4
+              @ticket = Ticket.new(booking_id: @booking.id, flight_id: @booking.flight_id, user_id: @booking.user_id, first_name: @booking.first_name_4,
+                last_name: @booking.last_name_4, pnr: @booking.pnr, price: @booking.price_4, seat_class: "economy"))
+              @ticket.save  
+            end           
+          end
           format.html {redirect_to @booking, notice: 'Flight was succesfully created'}
         else
           format.html {redirect_to :root}
@@ -53,7 +79,7 @@ class BookingsController < ApplicationController
 
   def booking_params
   	params.require(:booking).permit(:pnr, :user_id, :total_price, :departure_airport, :arrival_airport, :flight_id, :passenger_id,
-      :departure_date, :departure_time, :arrival_time, :flight_number, :first_name_1, :last_name_1, :first_name_2, :last_name_2, :first_name_3, :last_name_3, :first_name_4, :last_name_4,
-      :booking_class, :mail, :passengers)
+      :departure_date, :departure_time, :arrival_time, :flight_number, :first_name_1, :last_name_1, :first_name_2, :last_name_2, 
+      :first_name_3, :last_name_3, :first_name_4,:last_name_4, :price_1, :price_2, :price_3, :price_4,:booking_class, :mail, :passengers)
   end
 end
